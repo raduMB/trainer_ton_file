1ba5739f437a6a733e5280a3967488df6a884cea
Radu BOBES <radu-bobes_student2023@wilder.school>
2023-05-23 15:31:16 +0200

    second commit

--- form.php
+++ form.php
@@ -1,7 +1,5 @@
 <?php
 
-// final version
-
 if(isset($_POST["birthDate"]) && $_POST["birthDate"]== "") {
     $errors[] = "Birth date is required";
 }
@@ -60,77 +58,6 @@ if($_SERVER["REQUEST_METHOD"] === "POST" ){
     
 }
 
-
-
-
-/*
-
-
-// version multiple files
-
-
-
-if(!empty($_FILES['files']['name'][0])) {
-    
-    $files = $_FILES['files'];
-
-    $uploaded = array();
-    $failed = array();
-
-    $allowed = array('jpg','png', 'gif', 'webp');
-
-    foreach($files['name'] as $position => $file_name) {
-
-        $file_tmp = $files['tmp_name'][$position];
-        $file_size = $files['size'][$position];
-        $file_error = $files['error'][$position];
-
-        $file_ext = explode('.', $file_name);
-        $file_ext = strtolower(end($file_ext));
-
-        if(in_array($file_ext, $allowed)) {
-
-            if($file_error === 0) {
-
-                if($file_size <= 1048576) {
-
-                    $file_name_new = uniqid('', true) . '.' . $file_ext;
-                    $file_destination ='public/uploads/' . $file_name_new;
-
-                    if(move_uploaded_file($file_tmp, $file_destination)) {
-                        $uploaded[$position] = $file_destination;
-                } else {
-                        $failed[$position] = "[{$file_name}] failed ti upload.";
-                }
-                
-             } else {
-                    $failed[$position] = "[{$file_name}] is too large.";
-                }
-
-            } else {
-                $failed[$position] = "[{$file_name}] erored with code {$file_error}.";
-            }
-
-        } else {
-            $failed[$position] = "[{$file_name}] file extension '{$file_ext}' is not allowed.";
-        }
-
-    }
-
-    if(!empty($uploaded)) {
-        print_r($uploaded);
-    }
-
-    if(!empty($failed)) {
-        print_r($failed);
-    }
-
-
-}
-
-
-*/
-
 ?>
 
 
@@ -138,42 +65,40 @@ if(!empty($_FILES['files']['name'][0])) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drivers License</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Drivers License</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
 
-<body>
-
-    <header>
-        <h1>Drivers License ready to print</h1>
-        <img src="public/images/logo_license.jpg" alt="Logo License">
-    </header>
-
-    <main>
-        <div class="summary">
-            <p>
-                <img src="public/uploads/hommie.jpg" alt="">
-            </p> 
-            <ul>
-                <p>Name: Homer Simpson</p>
-                <P>Birth date : <span><?php echo htmlentities($_POST["birthDate"]); ?></span></p>
-                <p>Expires : 31/02/2100</p>
-                <P>Class : <span><?php echo htmlentities($_POST["class"]); ?></span></p>
-                <P>Address : in the middle of nowhere</p>
-                <P>Town : Springfield</p>
-                <P>ZIP code : 666</p>
-                <P>Sex : <span><?php echo htmlentities($_POST["sex"]); ?></span></p>
-                <P>Height : <span><?php echo htmlentities($_POST["height"]); ?></span></p>
-                <P>Weight : <span><?php echo htmlentities($_POST["weight"]); ?></span></p>
-                <P>Hair : <span><?php echo htmlentities($_POST["hair"]); ?></span></p>
-                <P>Eyes : <span><?php echo htmlentities($_POST["eyes"]); ?></span></p>
-            </ul>
-        </div>
-    </main>
-</body>
+    <body>
+        <header>
+            <h1>Drivers License ready to print</h1>
+            <img src="public/images/logo_license.jpg" alt="Logo License">
+        </header>
+        <main>
+            <div class="summary">
+                <p>
+                    <img src="public/uploads/hommie.jpg" alt="">
+                </p> 
+                <ul>
+                    <p>Name: Homer Simpson</p>
+                    <P>Birth date : <span><?php echo htmlentities($_POST["birthDate"]); ?></span></p>
+                    <p>Expires : 31/02/2100</p>
+                    <P>Class : <span><?php echo htmlentities($_POST["class"]); ?></span></p>
+                    <P>Address : in the middle of nowhere</p>
+                    <P>Town : Springfield</p>
+                    <P>ZIP code : 666</p>
+                    <P>Sex : <span><?php echo htmlentities($_POST["sex"]); ?></span></p>
+                    <P>Height : <span><?php echo htmlentities($_POST["height"]); ?></span></p>
+                    <P>Weight : <span><?php echo htmlentities($_POST["weight"]); ?></span></p>
+                    <P>Hair : <span><?php echo htmlentities($_POST["hair"]); ?></span></p>
+                    <P>Eyes : <span><?php echo htmlentities($_POST["eyes"]); ?></span></p>
+                </ul>
+            </div>
+        </main>
+    </body>
 
 </html>
